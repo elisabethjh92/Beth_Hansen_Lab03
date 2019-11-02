25b04fed8f9b404f8026ece4ef14a28cd45c4b43
Elisabeth Hansen <bethhansen92@gmail.com>
2019-11-02 15:48:07 -0700

    changed some js

--- app.js
+++ app.js
@@ -4,63 +4,63 @@ var visitorName = prompt('Hello! What is your name?');
 alert('Hello, ' + visitorName + '!' + ' Welcome to my website!')
 console.log(visitorName);
 
-// var bethBirth = prompt('Did I grow up in the US?');
-// bethBirth = bethBirth.toLowerCase();
+var bethBirth = prompt('Did I grow up in the US?');
+bethBirth = bethBirth.toLowerCase();
 
-// if (bethBirth === 'no' || bethBirth === 'n') {
-//   alert('That is correct!');
-//   console.log('That is correct!');
-// } else {
-//   alert('Not quite!');
-//   console.log('Not quite!');
-// }
+if (bethBirth === 'no' || bethBirth === 'n') {
+  alert('That is correct!');
+  console.log('That is correct!');
+} else {
+  alert('Not quite!');
+  console.log('Not quite!');
+}
 
-// var bethUni = prompt('Did I go to University in the US?');
-// bethUni = bethUni.toLowerCase();
+var bethUni = prompt('Did I go to University in the US?');
+bethUni = bethUni.toLowerCase();
 
-// if (bethUni === 'yes' || bethUni === 'y') {
-//   alert('That is correct!');
-//   console.log('Correct!'); 
-// } else {
-//   alert('Not quite!')
-//   console.log('Not quite!');
-// }
+if (bethUni === 'yes' || bethUni === 'y') {
+  alert('That is correct!');
+  console.log('Correct!'); 
+} else {
+  alert('Not quite!')
+  console.log('Not quite!');
+}
 
-// var bethPets = prompt('Do I have dogs?');
-// bethPets = bethPets.toLowerCase();
+var bethPets = prompt('Do I have dogs?');
+bethPets = bethPets.toLowerCase();
 
-// if(bethPets === 'no' || bethPets === 'n') {
-//    alert('That is correct!')
-//     console.log('That is correct!');
-// } else {
-// alert('Not quite!')
-// console.log('Not quite!');
-// }
+if(bethPets === 'no' || bethPets === 'n') {
+   alert('That is correct!')
+    console.log('That is correct!');
+} else {
+alert('Not quite!')
+console.log('Not quite!');
+}
 
-// var bethWork = prompt('Have I worked as a nanny?');
-// bethWork = bethWork.toLowerCase();
+var bethWork = prompt('Have I worked as a nanny?');
+bethWork = bethWork.toLowerCase();
 
-// if(bethWork === 'yes' || bethWork === 'y') {
-//    alert('That is correct!')
-//     console.log('That is correct!');
-// } else {
-// alert('Not quite!')
-// console.log('Not quite!');
-// }
+if(bethWork === 'yes' || bethWork === 'y') {
+   alert('That is correct!')
+    console.log('That is correct!');
+} else {
+alert('Not quite!')
+console.log('Not quite!');
+}
 
-// var bethCountry = prompt('Have I lived in more than one country?');
-// bethCountry = bethCountry.toLowerCase();
+var bethCountry = prompt('Have I lived in more than one country?');
+bethCountry = bethCountry.toLowerCase();
 
-// if(bethCountry === 'yes' || bethCountry === 'y') {
-//    alert('That is correct!')
-//     console.log('That is correct!');
-// } else {
-// alert('Not quite!')
-// console.log('Not quite!');
-// }
+if(bethCountry === 'yes' || bethCountry === 'y') {
+   alert('That is correct!')
+    console.log('That is correct!');
+} else {
+alert('Not quite!')
+console.log('Not quite!');
+}
 
+var bethTravel = prompt('How many countries do you think I have visited?')
 var travelNumberGuesses = 4
-var bethTravel = parseInt(prompt('How many countries do you think I have visited?'))
 console.log(typeof bethTravel);
 while (travelNumberGuesses > 0) {
 
